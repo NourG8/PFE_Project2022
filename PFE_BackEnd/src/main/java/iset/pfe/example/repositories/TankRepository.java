7ab4ce21979fae1669f453d9aa33034df013b53b
ASUS <ASUS@DESKTOP-SV6U1KV.mshome.net>
2022-02-26 17:05:34 +0100

    Ahmed_Add_tankRepository:correction

--- PFE_BackEnd/src/main/java/iset/pfe/example/repositories/TankRepository.java
+++ PFE_BackEnd/src/main/java/iset/pfe/example/repositories/TankRepository.java
@@ -1,9 +1,8 @@
 package iset.pfe.example.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import iset.pfe.example.entities.Tank;
 
-import iset.pfe.example.entities.Agriculteur;
-
-public interface TankRepository extends JpaRepository<Agriculteur,Integer>{
+public interface TankRepository extends JpaRepository<Tank,Integer>{
 
 }
