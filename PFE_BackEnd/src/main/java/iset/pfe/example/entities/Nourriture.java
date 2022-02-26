8bda90217912c541d173399b690e819f85d50c9b
AhmedBs <medamed777@gmail.com>
2022-02-26 19:55:29 +0100

    ahmed_add_nourritureEntity

---
+++ PFE_BackEnd/src/main/java/iset/pfe/example/entities/Nourriture.java
@@ -0,0 +1,63 @@
+package iset.pfe.example.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Nourriture  implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	private Integer idNourriture;
+	private String Intitule;
+	private int Quantite ;
+	private String Qualite;
+	
+	//constructors
+	
+	public Nourriture() {
+		super();
+	}
+	public Nourriture(Integer idNourriture, String intitule, int quantite, String qualite) {
+		super();
+		this.idNourriture = idNourriture;
+		Intitule = intitule;
+		Quantite = quantite;
+		Qualite = qualite;
+	}
+	
+	//getters and setters 
+	//id
+	public Integer getIdNourriture() {
+		return idNourriture;
+	}
+	public void setIdNourriture(Integer idNourriture) {
+		this.idNourriture = idNourriture;
+	}
+	//Intitule
+	public String getIntitule() {
+		return Intitule;
+	}
+	public void setIntitule(String intitule) {
+		Intitule = intitule;
+	}
+	//Quantite
+	public int getQuantite() {
+		return Quantite;
+	}
+	public void setQuantite(int quantite) {
+		Quantite = quantite;
+	}
+	//Qualite
+	public String getQualite() {
+		return Qualite;
+	}
+	public void setQualite(String qualite) {
+		Qualite = qualite;
+	}
+	
+
+	
+	
+}
