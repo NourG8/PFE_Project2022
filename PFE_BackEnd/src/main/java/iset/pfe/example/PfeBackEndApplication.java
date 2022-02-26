1df68818ae07917bb26c568072f4a439e3f5902f
NourG8 <nourgerfali08@gmail.com>
2022-02-26 21:23:07 +0100

    Nour_Add_VacheController

--- PFE_BackEnd/src/main/java/iset/pfe/example/PfeBackEndApplication.java
+++ PFE_BackEnd/src/main/java/iset/pfe/example/PfeBackEndApplication.java
@@ -1,18 +1,25 @@
 package iset.pfe.example;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import iset.pfe.example.entities.Agriculteur;
+import iset.pfe.example.entities.Vache;
 import iset.pfe.example.repositories.AgriculteurRepository;
+import iset.pfe.example.repositories.VacheRepository;
 
 @SpringBootApplication
 public class PfeBackEndApplication implements CommandLineRunner{
 
 	@Autowired
 	private AgriculteurRepository agriculteurRepository;
+	@Autowired
+	private VacheRepository vacheRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PfeBackEndApplication.class, args);
 	}
@@ -21,6 +28,9 @@ public class PfeBackEndApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Agriculteur a1=new Agriculteur("Ahmed", "Ben saber", "ahmed@gmail.com", "Bizerte", 11221122, 98565455, "Ahmed", "ahmed");
 		agriculteurRepository.save(a1);
+		Date date1=new Date(12/12/2016);
+		Vache v1=new Vache(180.00, "race 1", date1, "malade", 23.5);
+		vacheRepository.save(v1);
 	}
 
 }
---
+++ PFE_BackEnd/src/main/java/iset/pfe/example/web/VacheController.java
@@ -0,0 +1,69 @@
+package iset.pfe.example.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import iset.pfe.example.entities.Agriculteur;
+import iset.pfe.example.entities.Vache;
+import iset.pfe.example.repositories.VacheRepository;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class VacheController {
+
+	@Autowired
+	private VacheRepository vacheRepository;
+	
+	@RequestMapping(value="/vaches",method = RequestMethod.GET)
+	public List<Vache> getVaches(){
+		return vacheRepository.findAll();
+	}
+	
+		
+	@RequestMapping(value="/vaches/{idVache}",method = RequestMethod.GET)
+    public Vache getVache(@PathVariable Integer idVache) {
+		Optional<Vache> v = vacheRepository.findById(idVache);
+		if (v.isPresent()) { 
+			return v.get();
+		}else throw new RuntimeException("vache introuvable !!");
+	}
+	
+	
+	@RequestMapping(value="/vaches/{idVache}",method = RequestMethod.DELETE)
+	@ResponseBody
+	public void deleteVache(@PathVariable Integer idVache) {
+		Optional<Vache> v = vacheRepository.findById(idVache);
+				if (v.isPresent()) { 
+				vacheRepository.deleteById(idVache);
+		    }else throw new RuntimeException("Vache introuvable ! vous ne pouvez pas le supprimer !!");
+	}
+	
+	@RequestMapping(value="/vaches",method = RequestMethod.POST)
+		public Vache AddVache(@RequestBody Vache vache){
+		return vacheRepository.save(vache);
+	}
+	
+	@RequestMapping(value="/vaches/{idVache}",method = RequestMethod.PUT)
+	
+	public Vache EditVache(@PathVariable Integer idVache, @RequestBody Vache vache){
+      Vache v = vacheRepository.findById(idVache).orElseThrow(()->new ResourceNotFoundException("Cet vache n'existe pas"));
+    	v.setDateNaissance(vache.getDateNaissance()); 
+    	v.setEtat(vache.getEtat());
+    	v.setPoids(vache.getPoids());
+    	v.setQte_prodLait(vache.getQte_prodLait());
+    	v.setRace(vache.getRace());
+			
+	  	return v;
+    }
+	
+}
